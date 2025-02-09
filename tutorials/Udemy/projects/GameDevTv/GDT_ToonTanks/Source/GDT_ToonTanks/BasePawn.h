fa23a222155f0d17b289aae0b296f89796e96701
LordSahvith <bulletproofchild@gmail.com>
2025-02-08 20:28:19 -0700

    forward declare capsule component class

--- tutorials/Udemy/projects/GameDevTv/GDT_ToonTanks/Source/GDT_ToonTanks/BasePawn.h
+++ tutorials/Udemy/projects/GameDevTv/GDT_ToonTanks/Source/GDT_ToonTanks/BasePawn.h
@@ -9,21 +9,24 @@
 UCLASS()
 class GDT_TOONTANKS_API ABasePawn : public APawn
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
-public:
-	// Sets default values for this pawn's properties
-	ABasePawn();
+  public:
+    // Sets default values for this pawn's properties
+    ABasePawn();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+  protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+  public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+  private:
+    UPROPERTY()
+    class UCapsuleComponent* CapsuleComponent;
 };
